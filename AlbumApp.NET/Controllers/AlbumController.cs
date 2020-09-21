779de58b8b0fc602e2bf26dffe3ecb87dea5c202
Seun Adetunji <seanadetunji@hotmail.com>
2020-09-21 21:51:33 +0100

    Importing project

---
+++ AlbumApp.NET/Controllers/AlbumController.cs
@@ -0,0 +1,103 @@
+﻿using AlbumApp.NET.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlbumApp.NET.Controllers
+{
+    public class AlbumController : Controller
+    {
+        // GET: Album
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllAlbums());
+        }
+
+        IEnumerable<Album> GetAllAlbums()
+        {
+            using (DBModel db = new DBModel())
+            {
+                return db.Albums.ToList<Album>();
+            }
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Album amb = new Album();
+            if(id != 0)
+            {
+                using (DBModel db = new DBModel())
+                {
+                    amb = db.Albums.Where(x => x.AlbumID == id).FirstOrDefault<Album>();
+                }
+            }
+            return View(amb);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Album alb)
+        {
+            try
+            {
+                if (alb.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(alb.ImageUpload.FileName);
+                    string extension = Path.GetExtension(alb.ImageUpload.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    alb.ImagePath = "~/AppFiles/Images/" + fileName;
+                    alb.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
+                }
+                using (DBModel db = new DBModel())
+                {
+                    if(alb.AlbumID == 0)
+                    {
+                        db.Albums.Add(alb);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(alb).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllAlbums()), message = "Submitted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { success = false, message = ex.Message}, JsonRequestBehavior.AllowGet); 
+
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    Album alb = db.Albums.Where(x => x.AlbumID == id).FirstOrDefault<Album>();
+                    db.Albums.Remove(alb);
+                    db.SaveChanges();
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllAlbums()), message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ AlbumApp.NET/Scripts/Scripts.js
@@ -0,0 +1,94 @@
+﻿
+// function to show image
+function ShowImagePreview(imageUploader, previewImage) {
+    if (imageUploader.files && imageUploader.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr("src", e.target.result);
+        }
+        reader.readAsDataURL(imageUploader.files[0]); 
+    }
+}
+
+function jQueryAjaxPost(form) {
+    // validatio  is applied to form
+    $.validator.unobtrusive.parse(form);
+    // if form validation is valid
+    if ($(form).valid()) {
+        // 
+        var ajaxConfig = {
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+                if (response.success) {
+                    $("#firstTab").html(response.html);
+                    refreshAddNewTab($(form).attr("data-restUrl"), true);
+                    // success message
+                    $.notify(response.message, "success");
+                    if (typeof activatejQueryTable !== "undefined" && $.isFunction(activatejQueryTable))
+                        activatejQueryTable();
+                }
+                else {
+                    // error message
+                    $.notify(response.message, "error");
+
+                }
+            }
+        }
+        if ($(form).attr('enctype') == "multipart/form-data") {
+            ajaxConfig["contentType"] = false;
+            ajaxConfig["processData"] = false;
+        }
+        $.ajax(ajaxConfig);
+    
+    }
+    return false;
+}
+
+function refreshAddNewTab(resetUrl, showViewTab) 
+{
+    $.ajax({
+        type: 'GET',
+        url: resetUrl,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $("ul.nav.nav-tabs a:eq(1)").html("Add New");
+            if (showViewTab)
+                $("ul.nav.nav-tabs a:eq(0)").tab("show");
+        }
+    });
+}
+
+function Edit(url) {
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $("ul.nav.nav-tabs a:eq(1)").html("Edit");
+                $("ul.nav.nav-tabs a:eq(1)").tab("show");
+        }
+    });
+}
+
+function Delete(url) {
+    if (confirm("Are you sure you want to delete this album?") == true)
+    {
+        $.ajax({
+            type: 'POST',
+            url: url,
+            success: function (response) {
+                if (response.success) {
+                    $("#firstTab").html(response.html);
+                    $.notify(response.message, "warn");
+                    if (typeof activatejQueryTable !== "undefined" && $.isFunction(activatejQueryTable))
+                        activatejQueryTable(); 
+                }
+                else {
+                    $.notify(response.message, "error");
+                }
+            }
+        });
+    }
+}
